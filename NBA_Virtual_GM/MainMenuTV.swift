bb2ea4b89f0d8855ad35357cd08a90078e35219e
Josh Kahl <j7kahl@gmail.com>
2015-01-31 16:54:55 -0800

    intial commit

---
+++ NBA_Virtual_GM/MainMenuTV.swift
@@ -0,0 +1,24 @@
+//
+//  MainMenuTV.swift
+//  NBA_Virtual_GM
+//
+//  Created by Josh Kahl on 1/31/15.
+//  Copyright (c) 2015 Josh Kahl. All rights reserved.
+//
+
+import UIKit
+
+class MainMenuTV: UITableViewController
+{
+  
+  
+  override func viewDidLoad()
+  {
+    super.viewDidLoad()
+  }
+    
+  override func didReceiveMemoryWarning()
+  {
+    super.didReceiveMemoryWarning()
+  }
+}
---
+++ NBA_Virtual_GM/NBA_Virtual_GM/TeamSelectionVC.swift
@@ -0,0 +1,30 @@
+//
+//  TeamSelectionVC.swift
+//  NBA_Virtual_GM
+//
+//  Created by Josh Kahl on 1/31/15.
+//  Copyright (c) 2015 Josh Kahl. All rights reserved.
+//
+
+import UIKit
+
+class TeamSelectionVC: UITableViewController
+{
+
+  
+
+  
+  
+  
+  override func viewDidLoad()
+  {
+    super.viewDidLoad()
+    
+  }
+
+  
+  override func didReceiveMemoryWarning()
+  {
+      super.didReceiveMemoryWarning()
+  }
+}
